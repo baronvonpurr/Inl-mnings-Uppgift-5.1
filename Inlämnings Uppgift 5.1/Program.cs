14b8b3f553d6543e29ed9d8ac3de8bfa096a87a1
baronvonpurr <robinhood.svensson@hotmail.com>
2023-11-27 21:33:14 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Skriv in ett namn");
+                namn[i] = Console.ReadLine();
+            }
+            for (int i = 0;i < 5;i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
